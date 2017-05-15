ab11b55b7199cc46747ee75ab443e79114ab7418
Josh Waite <kalewaite@gmail.com>
2017-05-15 09:07:45 -0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { CalcResultsComponent } from './calc-results/calc-results.component';
+import { InputComponent } from './input/input.component';
+import { StateSelectorComponent } from './state-selector/state-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcResultsComponent,
+    InputComponent,
+    StateSelectorComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpModule
   ],
   providers: [],
